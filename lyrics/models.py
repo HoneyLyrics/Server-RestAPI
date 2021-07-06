fdf5fac0db4227c849d37e62b9cbf6ef30da4375
kimSooHyun950921 <soohyunkim950921@gmail.com>
2021-07-06 16:59:06 +0900

    Change Database Attribute

--- lyrics/models.py
+++ lyrics/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class Mood(models.Model):
     moodId = models.IntegerField(primary_key=True)
-    mood = models.CharField(max_length=30)
+    mood = models.CharField(max_length=50)
 
 class SongInfo(models.Model):
     songId = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=50)
-    artist = models.CharField(max_length=50)
+    title = models.CharField(max_length=300)
+    artist = models.CharField(max_length=300)
     imgURL = models.URLField(default='https://cdnimg.melon.co.kr/cm2/album/images/100/43/575/10043575_20210302112520_500.jpg/melon/resize/120/quality/80/optimize')
     mood1 = models.ForeignKey(Mood, related_name='mood1', blank=True, on_delete=models.CASCADE)
     mood2 = models.ForeignKey(Mood, related_name='mood2', blank=True, on_delete=models.CASCADE)
