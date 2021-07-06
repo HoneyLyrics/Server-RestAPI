7174eca535f09b48c16baeeff797ac1d868eb328
kimSooHyun950921 <soohyunkim950921@gmail.com>
2021-07-06 19:30:29 +0900

    Load Data to DB

--- lyrics/admin.py
+++ lyrics/admin.py
@@ -1,26 +1,38 @@
 from django.contrib import admin
-from .models import SongInfo, Mood, Lyrics
+from .models import SongInfo, Mood, Lyrics, SongMood
 
 # Register your models here.
-#class SongInfoAdmin(admin.ModelAdmin):
-#    def save_model(self, request, obj, form, change):
-#        obj.user = request.user
-#        super().save_model(request, obj, form, change)
-#    list_display = ['songId', 'title', 'artist', 'imagurl',]
-#    admin.site.register(SongInfo, SongInfoAdmin)
 
 
-# class MoodAdmin(admin.ModelAdmin):
-#     def save_model(self, request, obj, form, change):
-#         obj.user = request.user
-#         super().save_model(request, obj, form, change)
-#     list_display = ['moodId', 'mood', ]
-#     admin.site.register(Mood, MoodAdmin)
+class SongInfoAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+    list_display = ['songId', 'title', 'artist', 'imgURL', ]
 
 
-# class LyricsAdmin(admin.ModelAdmin):
-#     def save_model(self, request, obj, form, change):
-#         obj.user = request.user
-#         super().save_model(request, obj, form, change)
-#     list_display = ['songId', 'content', ]
-#     admin.site.register(Lyrics, LyricsAdmin)
+class MoodAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+    list_display = ['moodId', 'mood', ]
+
+
+class LyricsAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+    list_display = ['songId', 'content', ]
+
+
+class SongMoodAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+    list_display = ['songId', 'moodId', ]
+
+
+admin.site.register(SongMood, SongMoodAdmin)
+admin.site.register(Lyrics, LyricsAdmin)
+admin.site.register(SongInfo, SongInfoAdmin)
+admin.site.register(Mood, MoodAdmin)
