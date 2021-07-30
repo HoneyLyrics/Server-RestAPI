78d68674c442e7f82d0b57bf8a9b9a59d6e6d70f
kimSooHyun950921 <soohyunkim950921@gmail.com>
2021-07-30 19:52:07 +0900

    Change Capital word

--- common/urls.py
+++ common/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('check/', views.check.as_view(), name='check'),
+    path('check/', views.Check.as_view(), name='check'),
     path('register/', views.Register.as_view(), name='register'),
-    path('login/', views.login.as_view(), name='login'),
-    path('logout/', views.logout.as_view(), name='logout')
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout')
 ]
